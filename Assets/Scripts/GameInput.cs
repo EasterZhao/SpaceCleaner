4d29d09026971d99fe75d7442ccfcb154fb05871
EasterZhao <118179963+EasterZhao@users.noreply.github.com>
2023-02-01 23:01:15 +0000

    Improve Movement
    add new input system function

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameInput : MonoBehaviour
+{
+    public event EventHandler OnInteractAction;
+    private PlayerInputActons playerInputActons;
+    private void Awake() {
+        playerInputActons = new PlayerInputActons();
+        playerInputActons.Player.Enable();
+        playerInputActons.Player.Interact.performed += Interact_performed;
+    }
+    // if Player pressed E
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        //OnInteractAction?.Invoke(this,EventArgs.Empty);
+        if(OnInteractAction != null)
+        {
+        OnInteractAction(this,EventArgs.Empty);
+        }
+    }
+    
+    
+    public Vector2 GetMovementVectorNormalized()
+         {
+         Vector2 inputVector = playerInputActons.Player.Move.ReadValue<Vector2>();
+        // a vector keeps the same direction but its length is 1.0
+        inputVector = inputVector.normalized; 
+        return inputVector;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,57 +4,101 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private GameInput gameInput;
 
     private Animator animator;
 
 
     private bool isWalking;
-    // Start is called before the first frame update
+
+    private Vector3 lastInteractDir;
+
     void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-        // a vector keeps the same direction but its length is 1.0.
-        inputVector = inputVector.normalized;
-        // Limit Y-axis movement to 0.
+        HandleMovement();
+        HandleInteractions();
+
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-
-        //Sleek turnaround.Vector3 Slerp(Vector3 a, Vector3 b, float t);
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward,moveDir, Time.deltaTime * rotateSpeed );
-        
-
-        if(moveDir != Vector3.zero)
+        if (moveDir != Vector3.zero)
         {
-            animator.SetBool("IsWalking",true);
+            lastInteractDir = moveDir;
+        }
+        
+        float interactDistance = 10f;
+        if (Physics.Raycast(transform.position + transform.up * 0.75f, lastInteractDir, out RaycastHit raycastHit, interactDistance))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
         }
         else
         {
-            animator.SetBool("IsWalking",false);
+            Debug.Log("x");
         }
-        
-    }   
+
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        // Limit Y-axis movement to 0.
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float playerRadius = .5f;
+        float playerHeight = 4f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        // Sleek turnaround.Vector3 Slerp(Vector3 a, Vector3 b, float t);
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        // Players can slide from the edge when pressing AW, WD, etc. at the same time
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
+        // Adding character animation
+        if (moveDir != Vector3.zero)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
 }
