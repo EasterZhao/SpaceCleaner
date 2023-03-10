8a5a7b228bfebd2687d314fbd3e8c3d7fbf06637
KeiranMor <keiran.morrison99@gmail.com>
2023-03-10 10:52:41 +0000

    Update PlayOneShot.cs

--- Assets/Scripts/PlayOneShot.cs
+++ Assets/Scripts/PlayOneShot.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+public class PlayOneShot : MonoBehaviour
 {
-    // Use this for initialization
-    void Start()
-    {
+    public AudioClip soundEffect;
+    public GameObject objectToPickUp;
 
+    private AudioSource audioSource;
+
+    private bool hasPickedUpObject = false;
+
+    private void Start()
+    {
+        // Get the AudioSource component attached to this GameObject
+        audioSource = GetComponent<AudioSource>();
+
+        // Assign the sound effect to the AudioSource component
+        audioSource.clip = soundEffect;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (!hasPickedUpObject && objectToPickUp != null && objectToPickUp.activeSelf)
+        {
+            // Check if the object to pick up is within range and the player presses the pick up button (e.g. E key)
+            if (Vector3.Distance(transform.position, objectToPickUp.transform.position) <= 2f && Input.GetKeyDown(KeyCode.E))
+            {
+                // Play the sound effect once
+                audioSource.PlayOneShot(soundEffect);
 
+                // Set a flag to prevent the sound effect from playing again if the object is already picked up
+                hasPickedUpObject = true;
+
+                // Deactivate the object to pick up
+                objectToPickUp.SetActive(false);
+            }
+        }
     }
 }
